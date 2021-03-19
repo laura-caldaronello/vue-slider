60d553f28542a11e507a43deadb56ebd5313371f
Laura Caldaronello <caldaronello.l@gmail.com>
2021-03-19 14:58:37 +0100

    Prima di poter cliccare sui pallini

--- js/main.js
+++ js/main.js
@@ -2,15 +2,21 @@ var app = new Vue({
     el: '#root',
     data: {
         counter: 1,
+        minCounter: 1,
+        maxCounter: 5, /* devo capire se c'è un modo per generalizzare il 5 con il mio metodo, ma non credo perchè ancora non sono in grado di rilevare quanti file ci sono in una cartella */
         path: 'img/',
-        extension: '.jpg'
+        extension: '.jpg',
+        fill: ['far','fas'],
     },
     methods: {
         goLeft: function() {
-            (this.counter == 1) ? (this.counter = 5) : (this.counter -= 1) /* devo capire se c'è un modo per generalizzare il 5 con il mio metodo, ma non credo perchè ancora non sono in grado di rilevare quanti file ci sono in una cartella */
+            (this.counter == this.minCounter) ? (this.counter = this.maxCounter) : this.counter--;
         },
         goRight: function() {
-            (this.counter == 5) ? (this.counter = 1) : (this.counter += 1)
+            (this.counter == this.maxCounter) ? (this.counter = this.minCounter) : this.counter++;
+        },
+        choose: function() {
+            
         }
     }
 });
